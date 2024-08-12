2677a23838d90b79f7c1958126fadc1a6a95a095
chandan kumar NA <33998599+chandankumar-na@users.noreply.github.com>
2024-08-13 02:51:03 +0530

    added get users end point

--- app/routers/users.py
+++ app/routers/users.py
@@ -26,3 +26,10 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+    
+@router.get("/", response_model= List[schemas.User])
+def read_users( db: Session = Depends(get_db)):
+    db_users = crud.get_users(db)
+    if db_users is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_users
