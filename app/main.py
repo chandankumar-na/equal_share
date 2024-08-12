833bd9beafe87a5b0488f4a328bb05b705af2438
chandan kumar NA <33998599+chandankumar-na@users.noreply.github.com>
2024-08-13 03:37:15 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from .routers import users, groups, expenses
-from .database import engine
-from . import models
+from routers import users, groups, expenses
+from database import engine
+import models
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -10,3 +10,8 @@ app = FastAPI()
 app.include_router(users.router)
 app.include_router(groups.router)
 app.include_router(expenses.router)
+
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app)
