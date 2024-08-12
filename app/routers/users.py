56de9d406b893c02afa9ca669a75ba992ed6cb2c
chandan kumar NA <33998599+chandankumar-na@users.noreply.github.com>
2024-08-13 02:55:51 +0530

    Update users.py

--- app/routers/users.py
+++ app/routers/users.py
@@ -31,5 +31,5 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 def read_users( db: Session = Depends(get_db)):
     db_users = crud.get_users(db)
     if db_users is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Users not found")
     return db_users
